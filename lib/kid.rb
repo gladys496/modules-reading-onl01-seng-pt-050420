8e14d30b743534a61c22f0d1c25afdc770f4836d
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-21 21:19:07 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,7 +1,7 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid
+class Kid < FancyDance
    extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
   attr_accessor :name
